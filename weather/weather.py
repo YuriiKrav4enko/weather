e27ff56bb6e352c648a72ee6c17d12bad71915df
Yuri Kravchenko <yuriy.krav4enko28@gmail.com>
2024-08-09 11:11:31 +0300

    create project structure

---
+++ weather/weather.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.11
+from coordinates import get_coordinates
+from weather_api_service import get_weather
+from wheather_formatter import format_weather
+
+
+def main():
+    coordinates = get_coordinates()
+    weather = get_weather()
+    print(format_weather(weather))
+
+
+if __name__ == "__main__":
+    main()
